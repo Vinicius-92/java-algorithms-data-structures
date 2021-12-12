07d690d73e36553965978b09fc1558041b263cb4
Vinicius Augusto <vinicius_augusto_@msn.com>
2021-12-12 17:29:49 -0300

    Understading what does a n^2 function

---
+++ algodata/src/main/java/BigO/CountingChallenges/OnPOW2.java
@@ -0,0 +1,20 @@
+package BigO.CountingChallenges;
+
+public class OnPOW2 {
+
+    // This is a nested if, in this case we do not sum the n's we multiply them
+    // giving a new order of magnitude
+    // O(n^2)
+    static void logAllPairsOfArray (char[] input) {
+        for (int i = 1; i < input.length; i++) {
+            for (int j = 1; j < input.length; j++) {
+                System.out.printf("%c %c \n", input[i], input[j]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        char[] array = new char[] {'a', 'b', 'c', 'd', 'e'};
+        logAllPairsOfArray(array);
+    }
+}
