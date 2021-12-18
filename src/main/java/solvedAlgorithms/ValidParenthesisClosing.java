9b9953b74c6f8ed3c36d29f210c69d611c9943b7
Vinicius Augusto <vinicius_augusto_@msn.com>
2021-12-18 16:04:28 -0300

    Valid parenthesis closing

---
+++ src/main/java/solvedAlgorithms/ValidParenthesisClosing.java
@@ -0,0 +1,39 @@
+package solvedAlgorithms;
+
+import java.util.LinkedList;
+
+/*
+    Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+    determine if the input string is valid.
+    An input string is valid if:
+        Open brackets must be closed by the same type of brackets.
+        Open brackets must be closed in the correct order.
+ */
+public class ValidParenthesisClosing {
+      boolean isValid(String s) {
+        LinkedList<Character> myList = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{')
+                myList.push(s.charAt(i));
+            else {
+                if (myList.isEmpty()) return false;
+                if (!match(myList.pop(), s.charAt(i))) return false;
+            }
+        }
+        return myList.isEmpty();
+    }
+
+    public boolean match(char first, char second) {
+        return (first == '(' && second == ')')
+                || (first == '[' && second == ']')
+                || (first == '{' && second == '}');
+    }
+
+    public static void main(String[] args) {
+        ValidParenthesisClosing valid = new ValidParenthesisClosing();
+        String s = "(){}[]";
+        String s1 = "({[({[()]})]})";
+        System.out.println(valid.isValid(s));
+        System.out.println(valid.isValid(s1));
+    }
+}
